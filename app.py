1d3122811af83ada7a806f334e62e30a167849c5
mpstaton <mpstaton@gmail.com>
2017-12-07 16:54:10 -0600

    does this add the target file

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
+
+from resources.organization import Organization
 
 app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/snapshot_dev'
+db = SQLAlchemy(app)
+
+#this is Flask routing conentions without the use of 
+#the restful API library. 
+@app.route('/')
+	def home():
+
+@app.route('/organizations')
+	def get_all_organizations():
+		#get all organizations
 
 @app.route('/organizations', methods=['POST'])
-	def create_organization
+	def create_organization():
 		#create an organization with kwargs
 
+@app.route('/organization/<string:called>')
+	def get_organization_by_called():
+		#return a single organization object by what it is called		
 
-app.run(port=5000)
+#classes are implemented as a Resource object 
+#to activate the Flask Restful API framework.
+
+
+app.run(port=5000, debug=True)
