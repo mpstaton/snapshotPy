25f1337f22f2d3048b6a35af6ebe8bb32fc09745
mpstaton <mpstaton@gmail.com>
2017-12-12 14:33:47 -0800

    adding person model

--- models/person.py
+++ models/person.py
@@ -1,7 +1,10 @@
 import psycopg2
 
 class Person:
-	def __init__(self, called, givenName, surName, maidenName, birthDate, gender, hasUserAccount, isTeamMember, id):
+	def __init__(self, called, givenName=None,
+				 surName=None, maidenName=None,
+				 birthDate=None, gender=None,
+				 hasUserAccount=False, isTeamMember=False, id=None):
 		self.called = called
 		self.givenName = givenName
 		self.surName = surName
@@ -14,7 +17,7 @@ class Person:
 
 #the below code is an attempt to get scripting working with Postgres
 def save_to_db(self):
-	connection = psycopg2.connect(user='postgres', database='snapshotDev', host='localhost')
+	connection = psycopg2.connect(user='postgres', database='snapshot_dev', host='localhost')
 	with connection.cursor as cursor:
 		cursor.execute('INSERT INTO persons (called, givenName, surName, maidenName, birthDate, gender, hasUserAccount, isTeamMember) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
 			(self.called, self.givenName, self.surName, self.maidenName, self.birthDate, self.gender, self.hasUserAccount, self.isTeamMember))
