091543c1378e3b3a2b496302c8615f9946d7d996
mpstaton <mpstaton@gmail.com>
2017-12-12 15:05:21 -0800

    debugged SQL query to insert person

--- models/person.py
+++ models/person.py
@@ -1,26 +1,28 @@
 import psycopg2
 
+
 class Person:
-	def __init__(self, called, givenName=None,
-				 surName=None, maidenName=None,
-				 birthDate=None, gender=None,
-				 hasUserAccount=False, isTeamMember=False, id=None):
-		self.called = called
-		self.givenName = givenName
-		self.surName = surName
-		self.maidenName = maidenName
-		self.birthDate = birthDate
-		self.gender = gender
-		self.hasUserAccount = hasUserAccount
-		self.isTeamMember = isTeamMember
+    def __init__(self, called, givenName=None,
+                 surName=None, maidenName=None,
+                 birthDate=None, gender=None,
+                 hasUserAccount=False, isTeamMember=False, id=None):
+        self.called = called
+        self.givenName = givenName
+        self.surName = surName
+        self.maidenName = maidenName
+        self.birthDate = birthDate
+        self.gender = gender
+        self.hasUserAccount = hasUserAccount
+        self.isTeamMember = isTeamMember
 
-
-#the below code is an attempt to get scripting working with Postgres
-def save_to_db(self):
-	connection = psycopg2.connect(user='postgres', database='snapshot_dev', host='localhost')
-	with connection.cursor as cursor:
-		cursor.execute('INSERT INTO persons (called, givenName, surName, maidenName, birthDate, gender, hasUserAccount, isTeamMember) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
-			(self.called, self.givenName, self.surName, self.maidenName, self.birthDate, self.gender, self.hasUserAccount, self.isTeamMember))
-	
-	connection.commit()
-	connection.close()
+    # the below code is an attempt to get scripting working with Postgres
+    def save_to_db(self):
+        connection = psycopg2.connect(user='mpstaton', database='snapshot_dev', host='localhost')
+        with connection.cursor() as cursor:
+            cursor.execute(
+                'INSERT INTO persons (called, givenName, surName, maidenName, birthDate, gender, hasUserAccount, '
+                'isTeamMember) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                (self.called, self.givenName, self.surName, self.maidenName, self.birthDate, self.gender,
+                 self.hasUserAccount, self.isTeamMember))
+        connection.commit()
+        connection.close()
