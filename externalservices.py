569b297cf2ce124bf719e9546cf508a040aa5a1f
mpstaton <mpstaton@gmail.com>
2017-12-14 13:54:25 -0800

    twitter API constants from mpstaton's user account

---
+++ externalservices.py
@@ -0,0 +1,8 @@
+
+
+TWITTER_CONSUMER_KEY = '6zWWA8kpGpcTN0ZcLzKQqwjxi'
+TWITTER_CONSUMER_SECRET = 'fIM1QmUqupgCu8YDCcDk1z20OVxPzmzMNgXefWmVuKQ6aj9JBE'
+
+TWITTER_REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+TWITTER_ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+TWITTER_AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
