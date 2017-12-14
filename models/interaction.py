bf0ae6c9020fc0fb5c01d42b8f93e68a01f1c6c5
Weijia Ma <maw@Weijias-MacBook-Pro.local>
2017-12-14 10:37:26 -0800

    completed uuid[] conversion

--- models/interaction.py
+++ models/interaction.py
@@ -3,8 +3,9 @@ import psycopg2.extensions as exten
 import psycopg2.extras
 
 class Interaction:
+    # interactionMaterial_uuids and contactCard_uuids can't be None, as the adaptor of uuid[] doesn't adapt None type
     def __init__(self, location_uuid=None, interactionType=None, startTime=None, endTime=None,
-                 uuid=None, interactionMaterial_uuids=None, contactCard_uuids=None):
+                 uuid=None, interactionMaterial_uuids=[], contactCard_uuids=[]):
         self.location_uuid = location_uuid
         self.interactionType = interactionType
         self.startTime = startTime
@@ -14,7 +15,6 @@ class Interaction:
         self.contactCard_uuids = contactCard_uuids
 
     def addToDB(self):
-        #TODO: cannot adapt None type
         psycopg2.extras.register_uuid()
         with connect() as conn:
             with conn.cursor() as cursor:
@@ -29,4 +29,4 @@ class Interaction:
             with conn.cursor() as cursor:
                 cursor.execute('SELECT * FROM interactions WHERE uuid=%s', (uuid,))
                 row = cursor.fetchone()
-                return cls(row[1], row[2], row[3], row[4], row[5], row[6])
+                return cls(row[1], row[2], row[3], row[4], row[5], row[6], row[7])
--- py_scripts/create_interaction.py
+++ py_scripts/create_interaction.py
@@ -2,7 +2,6 @@
 from models.interaction import Interaction
 import uuid
 
-newInteraction = Interaction(interactionMaterial_uuids=[uuid.UUID('2cc7b063-aa77-404d-a270-fe036548a932'), uuid.UUID('d59d4a55-36fc-4757-94d5-4355404d47bb')],
-                             contactCard_uuids=[uuid.UUID('2cc7b063-aa77-404d-a270-fe036548a932'), uuid.UUID('d59d4a55-36fc-4757-94d5-4355404d47bb')])
+newInteraction = Interaction(interactionMaterial_uuids=[uuid.UUID('2cc7b063-aa77-404d-a270-fe036548a932'), uuid.UUID('d59d4a55-36fc-4757-94d5-4355404d47bb')])
 
 newInteraction.addToDB()
